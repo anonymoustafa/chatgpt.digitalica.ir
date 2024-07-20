37b2be33cd7e414c3dd3b64efd7800c3c06b5b96
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-07-20 12:34:34 -0400

    Remove target directory from repository

--- rust_backend/target/debug/rust_backend.d
+++
@@ -1,1 +0,0 @@
-/home/mostafa/Documents/local-repositories/chatgpt.digitalica.ir/rust_backend/target/debug/rust_backend: /home/mostafa/Documents/local-repositories/chatgpt.digitalica.ir/rust_backend/src/main.rs
